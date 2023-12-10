bc5e3a43d6f2aa810ba7f7a7b63492477be77fff
Fidelis0001 <72283400+Fidelis0001@users.noreply.github.com>
2023-12-10 13:35:12 +0100

    refactoring the code
    to make it work again. changing onclick to addEventListener for all the buttons

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const player = document.getElementById("player");
+const btn = document.querySelectorAll(".btn");
+
+btn.forEach((button) => {
+  button.addEventListener("click", function () {
+    playSongk(this.id);
+  });
+});
 
 function playSong(id) {
   player.src = `https://www.youtube.com/embed/${id}?autoplay=1`;
-  console.log("logs out", "the button is clicked");
 }
