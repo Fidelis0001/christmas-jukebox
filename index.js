fba7ff96283848139dcb27c45a8f3c8f2ea1fede
Fidelis0001 <72283400+Fidelis0001@users.noreply.github.com>
2023-12-10 12:57:39 +0100

    cleaning the code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const player = document.getElementById("player")
+const player = document.getElementById("player");
 
 function playSong(id) {
-  player.src = `https://www.youtube.com/embed/${id}?autoplay=1` 
-//   player.classList.add("hiddenFrame")
+  player.src = `https://www.youtube.com/embed/${id}?autoplay=1`;
 }
